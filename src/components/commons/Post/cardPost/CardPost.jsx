a09da58fd4b06badb819a4129a87f0b00442ea47
billyobregon <apptalentos@gmail.com>
2024-01-16 19:15:42 -0500

    se agregó completó la información de la card agregando los iconos de like y comentarios, ademas de etiqueta

--- src/components/commons/Post/cardPost/CardPost.jsx
+++ src/components/commons/Post/cardPost/CardPost.jsx
@@ -1,10 +1,12 @@
 // CardPost.js
-import React from "react";
+import { FcLike, FcComments } from "react-icons/fc";
+import { BiPurchaseTagAlt } from "react-icons/bi";
+
 import "./CardPost.css";
 
 const CardPost = ({ post }) => {
   const { title, text, likes, owner, image, tags } = post;
-  const { firstName, lastName, picture } = owner;
+  const { firstName, lastName, picture, id } = owner;
 
   return (
     // <div className="card">
@@ -22,34 +24,47 @@ const CardPost = ({ post }) => {
     //   <h3>{title}</h3>
     //   <hr />
 
-      <div className="card">
-        <div>
-          <img className="img-avatar" src={picture} />
-        </div>
-        <div className="card-text">
-          <img className="portada" src={image} />
-          <div className="title-total">
-            <div className="title">Autor: <span>{`${firstName} ${lastName}`}</span></div>
+    <div className="card">
+      <div>
+        <img className="img-avatar" src={picture} />
+      </div>
+      <div className="card-text">
+        <img className="portada" src={image} />
+        <div className="title-total">
+          <div className="title">
+            Autor: <span>{`${firstName} ${lastName}`}</span> 
+          </div>
+          <div className="btn-perfil ">
+          <a href={`https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca${id}`}>Ver perfil</a>
+          </div>
 
-            <div className="desc">
-            {text}
-            </div>
+          <div className="desc">{text}</div>
+     
 
-            <p>Tags: {tags && tags.map((tag) => <span key={tag}>{tag} </span>)}</p>
-            <div className="actions">
-              <button>
-                <i className="far fa-heart"></i>
-              </button>
-              <button>
-                <i className="far fa-envelope"></i>
-              </button>
-              <button>
-                <i className="fas fa-user-friends"></i>
-              </button>
+{/* Sesión de comentarios y like */}
+          <div className="like-container">
+            <div className="likes">
+              <div>
+                <FcLike />
+              </div>
+              <div> {likes} </div>
             </div>
+            <div className="likes">
+              <div>
+                <FcComments />
+              </div>
+              <div> {likes} </div>
+            </div>
+          </div>
+          <hr />
+          <div className="like-container"><p>
+          <BiPurchaseTagAlt /> {tags && tags.map((tag) => <span className="tag-button" key={tag}> {tag} </span>)}
+
+          </p>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
